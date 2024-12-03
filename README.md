aec4d5a1734d2d2955e94de620db866c467de267
dcknuth <36804076+dcknuth@users.noreply.github.com>
2024-12-02 22:01:02 -0800

    Day 3

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Brute force is fine and the day seems much easier than last year's day 1
 ## [Day 2: Red-Nosed Reports](day02.py)
 Brute forced, simple loop for part 1. For part 2 I just dropped every level and short circuited when a good run was found. Both parts ran in a second, so good enough.
 
-## Day 3: ???
+## [Day 3: Mull It Over](day03.py)
+Had to relearn regular expressions (for the 20th time), but after that it was OK
--- day03.py
+++ day03.py
@@ -1,9 +1,37 @@
 # Day 3 part 1
+import re
 
 filename = 'test03.txt'
-#filename = 'input03.txt'
+filename = 'input03.txt'
+#filename = 'test03-2.txt'
 
 with open(filename) as f:
-    ls = f.read().strip().split('\n')
+    ls = f.read().strip()
 
-print(ls)
+func_ex = r'mul\((\d{1,3}),(\d{1,3})\)'
+matches = re.findall(func_ex, ls, re.MULTILINE)
+
+total = 0
+for p1, p2 in matches:
+    total += int(p1) * int(p2)
+    #print(f"Parameters: {p1} {p2}")
+print(total)
+
+# part 2
+func_ex = r'(mul\((\d{1,3}),(\d{1,3})\))|(do)\(\)|(don\'t)\(\)'
+matches = re.findall(func_ex, ls, re.MULTILINE)
+
+total = 0
+summing = True
+for parts in matches:
+    if 'mul' in parts[0] and summing:
+        p1, p2 = parts[0][4:].split(',')
+        p2 = p2[:-1]
+        total += int(p1) * int(p2)
+    elif 'do' in parts[3]:
+        summing = True
+    elif "don't" in parts[4]:
+        summing = False
+    elif summing:
+        print("something's wrong", parts)
+print(total)
